ac09c704673440b92c399626c62a835410182cc2
Alex Bourret <alexandre.bourret@dataiku.com>
2025-05-07 15:22:01 +0200

    fix schema initialisation

--- python-agent-tools/add-item-to-list/tool.py
+++ python-agent-tools/add-item-to-list/tool.py
@@ -2,6 +2,7 @@ import dataiku
 from dataiku.llm.agent_tools import BaseAgentTool
 from safe_logger import SafeLogger
 from office365_client import Office365Session, Office365ListWriter
+from dss_constants import DSSConstants
 
 logger = SafeLogger("sharepoint-tool plugin")
 
@@ -9,7 +10,7 @@ logger = SafeLogger("sharepoint-tool plugin")
 class WriteToSharePointListTool(BaseAgentTool):
 
     def set_config(self, config, plugin_config):
-        logger.info('SharePoint Online plugin list write tool v{}'.format("0.0.1"))
+        logger.info('SharePoint Online plugin list write tool v{}'.format(DSSConstants.PLUGIN_VERSION))
 
         self.config = config
         connection_name = config.get("sharepoint_connection")
@@ -21,12 +22,13 @@ class WriteToSharePointListTool(BaseAgentTool):
         sharepoint_url = config.get("sharepoint_url")
 
         session = Office365Session(access_token=sharepoint_access_token)
-        site_id, list_id = session.extract_site_list_from_url(sharepoint_url)
+        site_id, self.list_id = session.extract_site_list_from_url(sharepoint_url)
         site = session.get_site(site_id)
-        self.list = site.get_list(list_id)
+        self.list = site.get_list(self.list_id)
         self.output_schema = None
+        self.output_schema, self.properties = self._get_schema_and_properties()
 
-    def get_descriptor(self, tool):
+    def _get_schema_and_properties(self):
         output_columns = []
         properties = {}
         required = []
@@ -43,38 +45,46 @@ class WriteToSharePointListTool(BaseAgentTool):
                     "type": "string",  # we don't have access to that information
                     "name": sharepoint_column.get("name")
                 })
-        self.output_schema = {
+        output_schema = {
             "columns": output_columns
         }
-        logger.info("Properties detected on this list: {}".format(properties))
+        return output_schema, properties
+
+    def get_descriptor(self, tool):
+        logger.info("Properties detected on this list: {}".format(self.properties))
         return {
             "description": "This tool can be used to access lists on SharePoint Online. The input to this tool is a dictionary containing the new issue summary and description, e.g. '{'summary':'new issue summary', 'description':'new issue description'}'",
             "inputSchema": {
                 "$id": "https://dataiku.com/agents/tools/search/input",
                 "title": "Add an item to a SharePoint Online list tool",
                 "type": "object",
-                "properties": properties
+                "properties": self.properties
             }
         }
 
     def invoke(self, input, trace):
+        logger.info("Invoke with schema {}".format(self.output_schema))
         sharepoint_writer = Office365ListWriter(
             self.list,
             self.output_schema,
             write_from_dict=True
         )
+        logger.info("Office365ListWriter initialised")
         row = input.get("input", {})
 
         # Log inputs and config to trace
         trace.span["name"] = "WRITE_TO_SHAREPOINT_LIST_TOOL_CALL"
-        trace.inputs["list"] = self.list
+        trace.inputs["list"] = self.list_id
         trace.inputs["row"] = row
         trace.attributes["config"] = self.config
 
+        logger.info("writing row")
         sharepoint_writer.write_row(row)
+        logger.info("closing writer")
         sharepoint_writer.close()
 
-        output_text = 'The record was added on the "{}" SharePoint list'.format(self.list)
+        output_text = 'The record was added on the "{}" SharePoint list'.format(self.list_id)
+        logger.info(output_text)
 
         # Log outputs to trace
         trace.outputs["output"] = output_text
