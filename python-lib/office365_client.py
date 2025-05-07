d6cea39fba7b97d302bf72ccb9f54f3400173ff6
Alex Bourret <alexandre.bourret@dataiku.com>
2025-05-07 17:34:30 +0200

    Adding error handling for site+list not found

--- python-lib/office365_client.py
+++ python-lib/office365_client.py
@@ -252,6 +252,8 @@ class Office365Session():
                 site_id = site.get("id")
             site_counter += 1
         logger.info("searched through {} sites, id found is '{}'".format(site_counter, site_id))
+        if not site_id:
+            raise Exception("The site '{}' was not found. Check the URL or your credentials.".format(site_name))
         site = self.get_site(site_id)
         list_id = None
         logger.info("searching for list '{}'".format(list_name))
@@ -261,6 +263,8 @@ class Office365Session():
                 list_id = sharepoint_list.get("id")
             list_counter += 1
         logger.info("searched through {} lists, id found is '{}'".format(list_counter, list_id))
+        if not list_id:
+            raise Exception("The list '{}' was not found. Check the URL or your credentials.".format(list_name))
         return site_id, list_id
 
     def search_list(self, query):
